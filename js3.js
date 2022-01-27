37df274444a550685a20b4c47965e836a1043fe8
iamadityaraj04 <adityarohitraj@gmail.com>
2022-01-27 16:53:53 +0530

    function in js

---
+++ js3.js
@@ -0,0 +1,17 @@
+// About funtion in js
+
+// function sayhello(){
+//     console.log("Hello Aditya...");
+// }
+// sayhello();
+
+// function say_hello(name){
+//     console.log("Hello! "+name);
+// }
+// say_hello("aditya");
+
+function say_hello(name){
+    console.log(`Hello! ${name}. How are you? `);
+}
+say_hello("aditya");
+
