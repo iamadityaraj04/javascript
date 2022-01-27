79b50999095140065c0793507ad5649125ea193f
iamadityaraj04 <adityarohitraj@gmail.com>
2022-01-27 16:53:38 +0530

    swapping two number

---
+++ js2.js
@@ -0,0 +1,11 @@
+// swapping two number
+
+var a=10, b=4;
+var c=b;
+b=a;
+a=c;
+console.log("a: "+a+" "+"b: "+b);
+
+// diff b/w == and ===
+//== : it checks only value
+//===: it checks both value and its data type
