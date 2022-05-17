b6141ff259c42a0a906491dcbbf741a9e67e1a72
iamadityaraj04 <adityarohitraj@gmail.com>
2022-05-17 23:23:52 +0530

    ES6JS

---
+++ ES6JS_templeteLiteral.js
@@ -0,0 +1,14 @@
+//templete literals
+
+let str1="Hello World";
+let str2=`
+Hello!
+
+Aditya
+            Welcome to advanced javascript.
+`
+console.log(str1);
+console.log(str2);
+
+let age=20;
+console.log(`Hello, i am aditya and i am ${age} years old.`);
